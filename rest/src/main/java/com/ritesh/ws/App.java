df9e2558695c0c7fa643b2a7cf8806e64340c591
ritesh705 <ritesh705@gmail.com>
2017-12-13 02:23:02 +0530

    pom updated.

---
+++ rest/src/main/java/com/ritesh/ws/App.java
@@ -0,0 +1,13 @@
+package com.ritesh.ws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
